a1d0835efee5a9922bc9b627eabd831803c57dc5
ivosantana <30847793+ivosantana@users.noreply.github.com>
2017-11-08 21:59:43 -0200

    Update urls.py

--- django_get_started/urls.py
+++ django_get_started/urls.py
@@ -19,6 +19,7 @@ urlpatterns = patterns('',
     url(r'^lista_cursos', 'app.views.lista_cursos', name='lista_cursos'),
     url(r'^lista_candidatos', 'app.views.lista_candidatos', name='lista_candidatos'),
     url(r'^lista_vestibulares', 'app.views.lista_vestibulares', name='lista_vestibulares'),
+    url(r'^lista_locais_prova', 'app.views.lista_locais_prova', name='lista_locais_prova'),
     url(r'^login/$',
         'django.contrib.auth.views.login',
         {
